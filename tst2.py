ad73d7f6300fea46e0c4d3bc6ef72e029b8d9211
averonque <averonque09@gmail.com>
2026-01-05 10:22:02 +0800

    Update tst2.py

--- tst2.py
+++ tst2.py
@@ -757,10 +757,10 @@ api_key = "weex_f67d25e3b8c4d7639e7deb7c558016bb"
 secret_key = "29056e6c4da2ea623bdfbf6fb223a48f7d192622e31803e6e64c5ceee3bc2611"
 access_passphrase = "weex652694794"
 
-def generate_signature(secret_key, timestamp, method, request_path, query_string, body):
-    message = timestamp + method.upper() + request_path + query_string + str(body)
-    signature = hmac.new(secret_key.encode(), message.encode(), hashlib.sha256).digest()
-    return base64.b64encode(signature).decode()
+def generate_signature_get(secret_key, timestamp, method, request_path, query_string):
+  message = timestamp + method.upper() + request_path + query_string
+  signature = hmac.new(secret_key.encode(), message.encode(), hashlib.sha256).digest()
+  return base64.b64encode(signature).decode()
 
 def get_usdt_balance() -> float:
     """
@@ -771,7 +771,7 @@ def get_usdt_balance() -> float:
 
     timestamp = str(int(time.time() * 1000))
 
-    signature = generate_signature(secret_key, timestamp,"GET", path, "")
+    signature = generate_signature_get(secret_key, timestamp,"GET", path, "")
  
     headers = {
         "ACCESS-KEY": API_KEY,
